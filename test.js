c48eb2a3690b7410846b26eac9921e4da686ced7
Sindre Sorhus <sindresorhus@gmail.com>
2014-11-23 11:03:23 +0700

    cleanup tests

--- test.js
+++ test.js
@@ -1,37 +1,29 @@
 'use strict';
 var assert = require('assert');
 var fs = require('fs');
+var path = require('path');
 var gutil = require('gulp-util');
 var mkdirp = require('mkdirp');
 var rimraf = require('rimraf');
-var path = require('path');
 var vulcanize = require('./');
 
-function copyTestFile (src, dest) {
-	fs.writeFileSync(dest, fs.readFileSync(src, 'utf8'), 'utf8');
+function copyTestFile(src, dest) {
+	fs.writeFileSync(dest, fs.readFileSync(src, 'utf8'));
 }
 
 describe('should vulcanize web components:', function (argument) {
 	var targets = ['', '/abc', '/xyz', '/xyz/abs'];
 
-	before(function(cb) {
-		rimraf('tmp', function (err) {
-    		if (err) {
-      			return cb(err);
-      		}
+	before(function () {
+		rimraf.sync('tmp');
+		mkdirp.sync('tmp');
 
-    		mkdirp.sync('tmp');
-
-    		targets.forEach(function(t) {
-    			var dest = path.join('tmp/src' + t);
-    			mkdirp.sync(dest);
-    			copyTestFile('./fixture/index.html', path.join(dest, 'index.html'));
-    			copyTestFile('./fixture/import.html', path.join(dest, 'import.html'));
-    		});
-
-    		cb();
-  		});
-
+		targets.forEach(function (el) {
+			var dest = path.join('tmp', 'src', el);
+			mkdirp.sync(dest);
+			copyTestFile('fixture/index.html', path.join(dest, 'index.html'));
+			copyTestFile('fixture/import.html', path.join(dest, 'import.html'));
+		});
 	});
 
 	it('single', function (cb) {
@@ -70,15 +62,15 @@ describe('should vulcanize web components:', function (argument) {
 
 		stream.on('data', function (file) {
 			var t = path.dirname(file.path).replace('tmp/build', '');
-			assert(targets.indexOf(t) !== -1);
-			assert.equal(file.relative, 'index.html');
+			assert.notStrictEqual(targets.indexOf(t), -1);
+			assert.strictEqual(file.relative, 'index.html');
 			assert(/Imported/.test(file.contents.toString()));
 		});
 
 		stream.on('end', cb);
 
-		targets.forEach(function(t) {
-			var src = path.join('tmp/src', t);
+		targets.forEach(function (el) {
+			var src = path.join('tmp', 'src', el);
 			stream.write(new gutil.File({
 				cwd: __dirname,
 				base: 'tmp/src',
